b64f4645f54a8c7319f32492ab3099797310d41b
LofiTea <iwilldiscoveraurora@gmail.com>
2024-05-31 14:47:27 +0900

    Update 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
-  showGraph: boolean,
+  showGraph: boolean
 }
 
 /**
@@ -23,7 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false,
+      showGraph: false
     };
   }
 
@@ -43,16 +43,14 @@ class App extends Component<{}, IState> {
       let x = 0;
       const interval = setInterval(() => {
           DataStreamer.getData((serverResponds: ServerRespond[]) => {
-              // Update the state by creating a new array of data that consists of
-              // Previous data in the state and the new data from server
               this.setState({
                   data: serverResponds,
-                  showGraph: true,
+                  showGraph: true
               });
           });
           x++;
           if (x > 1000) {
-              clearInterval(interval)
+              clearInterval(interval);
           }
       }, 100);
   }
